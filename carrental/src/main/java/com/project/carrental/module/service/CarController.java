fde6d0b7c0385694efd39a461c13b48106269b72
Uzytkownik <uzytkownik@kursGIT.pl>
2021-09-30 21:54:54 +0200

    not working car view

--- carrental/src/main/java/com/project/carrental/module/service/CarController.java
+++ carrental/src/main/java/com/project/carrental/module/service/CarController.java
@@ -1,43 +1,43 @@
-package com.project.carrental.module.service;
-
-import com.project.carrental.module.dto.CarDto;
-import com.project.carrental.module.mapper.CarMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-public class CarController {
-
-    @Autowired
-    private CarService carService;
-
-    @GetMapping(value = "/api/car/{id}")
-    public CarDto getCar(@PathVariable Long id) {
-        return CarMapper.map(carService.getOneById(id));
-    }
-
-
-
-    @GetMapping("/api/car")
-    public List<CarDto> getCars(
-            @RequestParam(required = false) String brand,
-            @RequestParam(required = false) String model,
-            @RequestParam(required = false) String bodyType,
-            @RequestParam(required = false) String colour,
-            @RequestParam(required = false) Long prodDate,
-            @RequestParam(required = false) Long mileage,
-            @RequestParam(required = false) Long amount,
-            @RequestParam(required = false) String status
-    ) {
-        return CarMapper.map(
-                carService.getAll(brand, model, bodyType, colour, prodDate, mileage, amount, status)
-        );
-    }
-
-}
-
+//package com.project.carrental.module.service;
+//
+//import com.project.carrental.module.dto.CarDto;
+//import com.project.carrental.module.mapper.CarMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//@RestController
+//public class CarController {
+//
+//    @Autowired
+//    private CarService carService;
+//
+//    @GetMapping(value = "/api/car/{id}")
+//    public CarDto getCar(@PathVariable Long id) {
+//        return CarMapper.map(carService.getOneById(id));
+//    }
+//
+//
+//
+//    @GetMapping("/api/car")
+//    public List<CarDto> getCars(
+//            @RequestParam(required = false) String brand,
+//            @RequestParam(required = false) String model,
+//            @RequestParam(required = false) String bodyType,
+//            @RequestParam(required = false) String colour,
+//            @RequestParam(required = false) Long prodDate,
+//            @RequestParam(required = false) Long mileage,
+//            @RequestParam(required = false) Long amount,
+//            @RequestParam(required = false) String status
+//    ) {
+//        return CarMapper.map(
+//                carService.getAll(brand, model, bodyType, colour, prodDate, mileage, amount, status)
+//        );
+//    }
+//
+//}
+//
---
+++ carrental/src/main/java/com/project/carrental/module/service/CarWebController.java
@@ -0,0 +1,45 @@
+package com.project.carrental.module.service;
+
+import com.project.carrental.module.dto.CarDto;
+import com.project.carrental.module.entity.CarEntity;
+import com.project.carrental.module.menu.MenuDto;
+import com.project.carrental.module.menu.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Controller
+public class CarWebController {
+
+
+    @Autowired
+    private MenuService menuService;
+    @Autowired
+    private CarService carService;
+
+    @GetMapping({
+            "/home/{id}",
+            "/home/{id}.html",
+            "/home/{id}.php"
+    })
+    public String getBlogPost(@PathVariable("id") Long id,
+                              Model model) {
+
+        CarEntity car = carService.getOneById(id);
+        List<MenuDto> menuItems = menuService.getMenu();
+
+        model.addAttribute("car", car);
+        model.addAttribute("menuItems", menuItems);
+
+        return "home/car.html";
+    }
+
+
+
+}
